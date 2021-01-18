7b2c94b042a72027bc5229a118b4b42c2854ab4f
oleksiit <oleksiit@wix.com>
2021-01-18 16:11:19 +0200

    Added saving/getting data from localStorage #pr

--- src/utils/getCountriesViaApi.ts
+++ src/utils/getCountriesViaApi.ts
@@ -2,11 +2,31 @@ import { Country } from '../../types';
 
 const axios = require('axios');
 
-export const getCountriesViaApi = async () => {
+const fetchData = async () => {
   const url = 'https://restcountries.eu/rest/v2/all';
   let result: Country[];
   const response = await axios.get(url);
   result = response.data;
   return result;
+}
+
+export const getCountriesViaApi = async () => {
+  let lastRecordDate: number = 0;
+
+  if (window.localStorage.length === 0) {
+    const response = await fetchData();
+    window.localStorage.setItem('countries', JSON.stringify(response));
+    lastRecordDate = Date.now();
+  }
+  else {
+    if ((lastRecordDate - Date.now()) < 1000 * 60 * 60 * 24 * 3) {
+      return JSON.parse(window.localStorage.getItem('countries') as string);
+    }
+    else {
+      lastRecordDate = Date.now();
+      const response = await fetchData();
+      window.localStorage.setItem('countries', JSON.stringify(response));
+    }
+  }
 };
 
